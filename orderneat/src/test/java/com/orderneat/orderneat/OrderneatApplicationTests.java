076cf532be613f0ab5b232b9bf57914f94f28bed
DataJlee <jaek1997@gmail.com>
2022-11-24 00:20:41 +0900

    first commit

---
+++ orderneat/src/test/java/com/orderneat/orderneat/OrderneatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderneat.orderneat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderneatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
