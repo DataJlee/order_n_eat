f2a61c8d03e1005d478bd751aa9385952a7b6c4b
DataJlee <jaek1997@gmail.com>
2022-12-09 01:25:59 +0900

    @EnableJpaAuditing 애노테이션 추가

--- orderneat/src/main/java/com/orderneat/orderneat/OrderneatApplication.java
+++ orderneat/src/main/java/com/orderneat/orderneat/OrderneatApplication.java
@@ -2,8 +2,10 @@ package com.orderneat.orderneat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OrderneatApplication {
 
 	public static void main(String[] args) {
