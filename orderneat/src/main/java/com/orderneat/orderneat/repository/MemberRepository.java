f0a2154e1f3931d660dc94d64de4db07167e5d83
DataJlee <jaek1997@gmail.com>
2022-12-09 01:30:03 +0900

    회원repository 최초 커밋

---
+++ orderneat/src/main/java/com/orderneat/orderneat/repository/MemberRepository.java
@@ -0,0 +1,34 @@
+package com.orderneat.orderneat.repository;
+
+import com.orderneat.orderneat.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member fineOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findMemberByEmail(String email){
+        return em.createQuery("select m from Member m where m.email = :email", Member.class)
+                .setParameter("email", email)
+                .getResultList();
+    }
+}
