f687eb5f8a1db52111c63ea11feefb6732df47bf
DataJlee <jaek1997@gmail.com>
2022-12-10 01:12:18 +0900

    메뉴 리포지토리 최초 커밋

---
+++ orderneat/src/main/java/com/orderneat/orderneat/repository/MenuRepository.java
@@ -0,0 +1,28 @@
+package com.orderneat.orderneat.repository;
+
+import com.orderneat.orderneat.domain.Menu;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MenuRepository {
+
+    private final EntityManager em;
+
+    public void save(Menu menu){
+        if (menu.getId() == null){
+            em.persist(menu);
+        }else {
+            em.merge(menu);
+        }
+    }
+
+    public List<Menu> findAll(){
+        return em.createQuery("select m from Menu m", Menu.class)
+                .getResultList();
+    }
+}
