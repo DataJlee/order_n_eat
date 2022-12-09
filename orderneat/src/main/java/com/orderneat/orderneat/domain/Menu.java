45eb27f852471eb1eee5ef1bdb4fa4cebfc2389d
DataJlee <jaek1997@gmail.com>
2022-12-10 01:11:00 +0900

    menuImgUrl 임시주석처리 및 menuStatus 필드 추가

--- orderneat/src/main/java/com/orderneat/orderneat/domain/Menu.java
+++ orderneat/src/main/java/com/orderneat/orderneat/domain/Menu.java
@@ -1,12 +1,14 @@
 package com.orderneat.orderneat.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor
 public class Menu extends BaseTimeEntity{
 
     @Id
@@ -22,8 +24,18 @@ public class Menu extends BaseTimeEntity{
 
     private Integer price;
 
-    private String menuImgUrl;
+//    private String menuImgUrl;
 
     private String desc;
 
+    @Enumerated(EnumType.STRING)
+    private MenuStatus status;
+
+    public Menu(Store store, String category, Integer price, String desc, MenuStatus status){
+        this.store = store;
+        this.category = category;
+        this.price = price;
+        this.desc = desc;
+        this.status = status;
+    }
 }
