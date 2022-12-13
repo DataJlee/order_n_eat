67421b934791d99b14dc33465b98741a992497f8
DataJlee <jaek1997@gmail.com>
2022-12-14 01:16:28 +0900

    최초 커밋

---
+++ orderneat/src/main/java/com/orderneat/orderneat/service/StoreService.java
@@ -0,0 +1,12 @@
+package com.orderneat.orderneat.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StoreService {
+}
