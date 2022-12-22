6c769713fd24824b500f1fffa8b1b4d388da0c00
DataJlee <jaek1997@gmail.com>
2022-12-23 00:51:41 +0900

    성별, 생년월일 필드 제거 후 주소 필드 추가(Embedded) / createMember(회원생성메서드) 정적팩토리메서드 추가

--- orderneat/src/main/java/com/orderneat/orderneat/domain/Member.java
+++ orderneat/src/main/java/com/orderneat/orderneat/domain/Member.java
@@ -1,8 +1,10 @@
 package com.orderneat.orderneat.domain;
 
+import com.orderneat.orderneat.dto.MemberJoinFormDTO;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
 
@@ -18,24 +20,37 @@ public class Member extends BaseTimeEntity{
     @Column(unique = true)
     private String email;
 
+    private String name;
+
     private String password;
 
-    private String gender;
-
-    private String yearOfBirth;
+    @Embedded
+    private Address address;
 
     @Enumerated(EnumType.STRING)
     private Role role;
 
     @Builder
-    public Member(String email, String password, String gender, String yearOfBirth){
+    public Member(String email, String name, String password, Address address){
         this.email = email;
+        this.name = name;
         this.password = password;
-        this.gender = gender;
-        this.yearOfBirth = yearOfBirth;
+        this.address = address;
         this.role = Role.USER;
     }
 
     protected Member() {
+    }
+
+    public static Member createMember(MemberJoinFormDTO memberJoinFormDTO, PasswordEncoder passwordEncoder){
+        Address address = new Address(memberJoinFormDTO.getPostalCode(), memberJoinFormDTO.getAddress1(),
+                memberJoinFormDTO.getAddress2());
+        Member member = Member.builder()
+                .email(memberJoinFormDTO.getEmail())
+                .name(memberJoinFormDTO.getName())
+                .password(passwordEncoder.encode(memberJoinFormDTO.getPassword()))
+                .address(address)
+                .build();
+        return member;
     }
 }
