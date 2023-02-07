1da2d7d628856f826dd4029475d4baa638657296
John's MacBook Pro <johnlee@Johns-MacBook-M1-Pro-14.local>
2023-02-08 00:28:09 +0900

    mvc controller 추가

--- orderneat/src/main/java/com/orderneat/orderneat/controller/MemberController.java
+++ orderneat/src/main/java/com/orderneat/orderneat/controller/MemberController.java
@@ -1,9 +1,10 @@
 package com.orderneat.orderneat.controller;
 
-import com.orderneat.orderneat.domain.Address;
 import com.orderneat.orderneat.domain.Member;
 import com.orderneat.orderneat.dto.MemberJoinFormDTO;
+import com.orderneat.orderneat.dto.StoreRegistFormDTO;
 import com.orderneat.orderneat.service.MemberService;
+import com.orderneat.orderneat.service.StoreService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,61 +14,69 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/member")
-public class MemberController {
+
+public class StoreMemberController {
 
     public final MemberService memberService;
-
+    public final StoreService storeService;
     public final PasswordEncoder passwordEncoder;
 
-    @GetMapping("/memberInfo")
-    public String memberInfo(Model model){
-        log.info("called getMemberInfoPage()");
-        //로그인 상태 아님
-        if (true){
-            log.info("로그인 상태 아님 : loginPage로 redirect 중..");
-            return "redirect:/member/login";
-        }else{
-            log.info("로그인 상태 완료 : memberInfoPage 가져오는중..");
-            return "member/memberInfoPage";
-        }
+    @GetMapping("/store_member/login")
+    public String login(){
+        return "store_member/member-login-pg";
     }
 
-    @GetMapping("/login")
-    public String login(ModelAndView mv){
-        return "member/memberLoginPage";
+    @GetMapping("/store_member/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
+        return "store_member/member-login-pg";
     }
 
-    @GetMapping("/join")
+    @GetMapping("/store_member/join")
     public String joinForm(Model model){
         model.addAttribute("memberJoinFormDto", new MemberJoinFormDTO());
-        return "member/memberJoinPage";
+        return "store_member/member-join-pg";
     }
 
-    @PostMapping("/join")
+    @PostMapping("/store_member/join")
     public String memberJoin(@ModelAttribute("memberJoinFormDto") @Valid MemberJoinFormDTO memberJoinFormDto, BindingResult bindingResult, Model model){
         log.info("회원가입 페이지 가입하기 SUBMIT");
         if (bindingResult.hasErrors()){
             log.info(bindingResult.toString());
-            return "member/memberJoinPage";
+            return "store_member/member-join-pg";
         }
         try{
             Member member = Member.createMember(memberJoinFormDto, passwordEncoder);
             memberService.join(member);
         }catch (IllegalStateException e){
             model.addAttribute("errorMessage", e.getMessage());
-            return "member/memberJoinPage";
+            return "store_member/member-join-pg";
         }
-        return "/member/memberJoinSuccessPage";
+        return "store_member/member-join-success-pg";
     }
 
+    @GetMapping("/store_member/manage")
+    public String managePage(){
+        return "store_member/store-manage-page";
+    }
+
+
+    @GetMapping("/store_member/manage/store_info")
+    public String storeRegistForm(Model model, HttpServletRequest httpServletRequest){
+        model.addAttribute("storeRegistFormDto", new StoreRegistFormDTO());
+        return "store_member/store-info-manage-pg";
+    }
+
+//    @PostMapping("/store_member/manage/store_info")
+//    public String storeInfo(@ModelAttribute("storeRegistFormDto") StoreRegistFormDTO storeRegistFormDto){
+//
+//    }
 
 }
