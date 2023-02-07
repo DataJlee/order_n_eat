f9eaaa138263c926328a6e9cf62694aa8a69e0e8
Jae Lee <=>
2023-02-08 00:54:22 +0900

    깃 테스트

--- orderneat/src/main/java/com/orderneat/orderneat/api/MemberApi.java
+++ orderneat/src/main/java/com/orderneat/orderneat/api/MemberApi.java
@@ -1,20 +1,14 @@
 package com.orderneat.orderneat.api;
 
-import com.orderneat.orderneat.domain.Member;
-import com.orderneat.orderneat.service.MemberService;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotEmpty;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class MemberApi {
-    
+
 }
