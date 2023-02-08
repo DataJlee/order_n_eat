bac5275f9216c476a111b5f2c4cd35481897524f
Jae Lee <=>
2023-02-09 02:02:01 +0900

    회원가입 API 최초커밋

--- orderneat/src/main/java/com/orderneat/orderneat/api/MemberApi.java
+++ orderneat/src/main/java/com/orderneat/orderneat/api/MemberApi.java
@@ -1,9 +1,19 @@
 package com.orderneat.orderneat.api;
 
+import com.orderneat.orderneat.domain.Member;
+import com.orderneat.orderneat.domain.Role;
+import com.orderneat.orderneat.dto.member.MemberJoinRequest;
+import com.orderneat.orderneat.dto.member.MemberJoinResponse;
+import com.orderneat.orderneat.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 
 @RestController
@@ -11,4 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class MemberApi {
 
+    private final MemberService memberService;
+
+    @PostMapping("/member/v1.0/join")
+    public ResponseEntity<MemberJoinResponse> joinMemberByEmail(@RequestBody @Valid MemberJoinRequest request){
+        MemberJoinResponse response = memberService.join(request);
+        return ResponseEntity.ok(response);
+    }
 }
