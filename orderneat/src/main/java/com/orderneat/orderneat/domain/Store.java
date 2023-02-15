dec2163962fbb5918051841c6e4fbf18e54ac513
Jae Lee <=>
2023-02-16 02:43:47 +0900

    주소타입 Address -> String 임시 변경

--- orderneat/src/main/java/com/orderneat/orderneat/domain/Store.java
+++ orderneat/src/main/java/com/orderneat/orderneat/domain/Store.java
@@ -1,6 +1,6 @@
 package com.orderneat.orderneat.domain;
 
-import com.orderneat.orderneat.dto.StoreRegistFormDTO;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,8 +24,10 @@ public class Store extends BaseTimeEntity{
     @Column(name = "store_category")
     private String category;
 
-    @Embedded
-    private Address address;
+//    @Embedded
+//    private Address address;
+    @Column(name = "store_address")
+    private String address;
 
     @Column(name = "store_contact")
     private String contact;
@@ -35,18 +37,12 @@ public class Store extends BaseTimeEntity{
     private StoreStatus status;
 
     public boolean isOpen(){
-        boolean isOpen = false;
-        if (status == StoreStatus.OPEN) {
-            isOpen = true;
-        }
-        else if (status == StoreStatus.CLOSED) {
-            isOpen = false;
-        }
+        boolean isOpen = status == StoreStatus.OPEN;
         return isOpen;
     }
 
     @Builder
-    public Store(Long id, String name, String category, String contact, Address address) {
+    public Store(Long id, String name, String category, String contact, String address) {
         this.id = id;
         this.name = name;
         this.category = category;
@@ -55,15 +51,15 @@ public class Store extends BaseTimeEntity{
         this.status = StoreStatus.OPEN;
     }
 
-    public static Store createStore(StoreRegistFormDTO storeRegistFormDTO){
-        Address address = new Address(storeRegistFormDTO.getPostalCd(),
-                storeRegistFormDTO.getAddress1(), storeRegistFormDTO.getAddress2());
-        Store store = Store.builder()
-                .name(storeRegistFormDTO.getName())
-                .category(storeRegistFormDTO.getCategory())
-                .contact(storeRegistFormDTO.getContact())
-                .address(address)
-                .build();
-        return store;
-    }
+//    public static Store createStore(StoreRegistFormDTO storeRegistFormDTO){
+//        Address address = new Address(storeRegistFormDTO.getPostalCd(),
+//                storeRegistFormDTO.getAddress1(), storeRegistFormDTO.getAddress2());
+//        Store store = Store.builder()
+//                .name(storeRegistFormDTO.getName())
+//                .category(storeRegistFormDTO.getCategory())
+//                .contact(storeRegistFormDTO.getContact())
+//                .address(address)
+//                .build();
+//        return store;
+//    }
 }
