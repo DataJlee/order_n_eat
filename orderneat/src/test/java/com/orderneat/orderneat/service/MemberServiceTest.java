029e4d758b4bae3d33cd4c15eaa63d01fe877282
Jae Lee <=>
2023-02-16 02:40:22 +0900

    회원가입, 회원중복검증 테스트 코드 추가

--- orderneat/src/test/java/com/orderneat/orderneat/service/MemberServiceTest.java
+++ orderneat/src/test/java/com/orderneat/orderneat/service/MemberServiceTest.java
@@ -1,8 +1,9 @@
 package com.orderneat.orderneat.service;
 
 import com.orderneat.orderneat.domain.Member;
+import com.orderneat.orderneat.domain.Role;
 import com.orderneat.orderneat.dto.member.MemberJoinRequest;
-import com.orderneat.orderneat.dto.member.MemberJoinResponse;
+import com.orderneat.orderneat.exception.member.DuplicateMemberException;
 import com.orderneat.orderneat.repository.MemberRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Random;
 
 import static org.junit.Assert.*;
 
@@ -29,28 +29,39 @@ public class MemberServiceTest {
     PasswordEncoder passwordEncoder;
 
     @Test
-    public void join() throws Exception{
+    public void join() {
         //given
         MemberJoinRequest req = new MemberJoinRequest();
-        req.setEmail("jaek1994@gmail.com");
-        req.setPassword("1234");
-        Member member = Member.createMember(req, passwordEncoder);
+        req.setEmail("new@gmail.com");
+        req.setPassword("pass1234");
         //when
-        MemberJoinResponse res = memberService.join(req);
+        Long savedId = memberService.join(req);
+        //then
+        assertEquals(savedId, memberRepository.findOne(savedId).getId());
     }
 
-    @Test(expected = IllegalStateException.class)
-    public void validateDuplicateMember() throws Exception{
+    @Test(expected = DuplicateMemberException.class)
+    public void validateDuplicateMember() throws DuplicateMemberException {
         //given
         MemberJoinRequest req = new MemberJoinRequest();
         req.setEmail("jaek1997@gmail.com");
         req.setPassword("1234");
-        MemberJoinRequest req2 = new MemberJoinRequest();
-        req.setEmail("jaek1997@gmail.com");
-        req.setPassword("1234");
-        //when
+
         memberService.join(req);
+        //when
+        MemberJoinRequest req2 = new MemberJoinRequest();
+        req2.setEmail("jaek1997@gmail.com");
+        req2.setPassword("1234");
+
+        Member member2 = Member.builder()
+                .email(req2.getEmail())
+                .password(passwordEncoder.encode(req2.getPassword()))
+                .role(Role.USER)
+                .build();
+
         memberService.join(req2);
+
+        memberService.validateDuplicateMember(member2);
         //then
         fail("예외 발생 x, 테스트 실패");
     }
