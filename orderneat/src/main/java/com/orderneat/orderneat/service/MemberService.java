3caec5c27f1d875bb3ed6b92d8b7f9297071e0c4
Jae Lee <=>
2023-02-16 02:39:54 +0900

    서비스가 dto가 아닌 domain을 반환하도록 수정

--- orderneat/src/main/java/com/orderneat/orderneat/service/MemberService.java
+++ orderneat/src/main/java/com/orderneat/orderneat/service/MemberService.java
@@ -3,30 +3,27 @@ package com.orderneat.orderneat.service;
 import com.orderneat.orderneat.domain.Member;
 import com.orderneat.orderneat.domain.Role;
 import com.orderneat.orderneat.dto.member.MemberJoinRequest;
-import com.orderneat.orderneat.dto.member.MemberJoinResponse;
+import com.orderneat.orderneat.exception.member.DuplicateMemberException;
 import com.orderneat.orderneat.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class MemberService implements UserDetailsService {
+public class MemberService{
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public MemberJoinResponse join(final MemberJoinRequest memberJoinRequest){
+    public Long join(final MemberJoinRequest memberJoinRequest) {
+
         Member member = Member.builder()
                     .email(memberJoinRequest.getEmail())
                     .password(passwordEncoder.encode(memberJoinRequest.getPassword()))
@@ -34,37 +31,19 @@ public class MemberService implements UserDetailsService {
                     .build();
         validateDuplicateMember(member);
         memberRepository.save(member);
-        MemberJoinResponse memberJoinResponse = new MemberJoinResponse();
-        memberJoinResponse.setId(member.getId());
-        return memberJoinResponse;
+
+        return member.getId();
     }
 
-    private void validateDuplicateMember(Member member) {
+    public void validateDuplicateMember(Member member) {
+
         List<Member> memberByEmail = memberRepository.findByEmail(member.getEmail());
         if (!memberByEmail.isEmpty()){
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
+            throw new DuplicateMemberException();
         }
     }
 
-    public List<Member> findAllMembers(){
-        return memberRepository.findAll();
-    }
 
-    public Member findMember(Long id){
-        return memberRepository.findOne(id);
-    }
 
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        List<Member> member = memberRepository.findByEmail(email);
-        Member memberFound = member.get(0);
-        if (member.isEmpty()){
-            throw new UsernameNotFoundException(email);
-        }
-        return User.builder()
-                .username(memberFound.getEmail())
-                .password(memberFound.getPassword())
-                .roles(memberFound.getRole().toString())
-                .build();
-    }
+
 }
