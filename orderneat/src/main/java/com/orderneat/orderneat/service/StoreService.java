3f7a7756f0d38e5e9af76308125de79ddea59e64
Jae Lee <=>
2023-02-16 02:45:58 +0900

    save 메서드 단순화

--- orderneat/src/main/java/com/orderneat/orderneat/service/StoreService.java
+++ orderneat/src/main/java/com/orderneat/orderneat/service/StoreService.java
@@ -2,11 +2,12 @@ package com.orderneat.orderneat.service;
 
 
 import com.orderneat.orderneat.domain.Store;
+import com.orderneat.orderneat.dto.store.StoreRegisterRequest;
+import com.orderneat.orderneat.dto.store.StoreRegisterResponse;
 import com.orderneat.orderneat.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 @RequiredArgsConstructor
@@ -16,8 +17,17 @@ public class StoreService {
     private final StoreRepository storeRepository;
 
     @Transactional
-    public void save(Store store){
+    public StoreRegisterResponse save(final StoreRegisterRequest request) {
+        Store store = Store.builder()
+                .name(request.getStoreName())
+                .category(request.getStoreCategory())
+                .address(request.getStoreAddress())
+                .address(request.getStoreContact())
+                .build();
         storeRepository.save(store);
+        StoreRegisterResponse response = new StoreRegisterResponse();
+        response.setId(store.getId());
+        return response;
     }
 
 
