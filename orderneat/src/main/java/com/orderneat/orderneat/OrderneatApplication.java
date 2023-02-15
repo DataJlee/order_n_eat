0d971a5d441044809e2df2c1ed8ca8a897dedca3
Jae Lee <=>
2023-02-16 02:41:35 +0900

    yamlConfig 클래스에서 환경체크 변수 스크립트로 볼 수 있게 설정

--- orderneat/src/main/java/com/orderneat/orderneat/OrderneatApplication.java
+++ orderneat/src/main/java/com/orderneat/orderneat/OrderneatApplication.java
@@ -1,15 +1,34 @@
 package com.orderneat.orderneat;
 
+import com.orderneat.orderneat.config.YamlConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.yaml.snakeyaml.Yaml;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class OrderneatApplication {
+@Slf4j
+public class OrderneatApplication implements CommandLineRunner {
+
+	@Autowired
+	private YamlConfig myConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderneatApplication.class, args);
 	}
 
+	public void run(String... args) throws Exception {
+		System.out.println("using environment: " + myConfig.getEnvironment());
+		System.out.println("name: " + myConfig.getName());
+		System.out.println("enabled:" + myConfig.isEnabled());
+		System.out.println("servers: " + myConfig.getServers());
+	}
 }
