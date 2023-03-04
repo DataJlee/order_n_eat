47c471114b5f62924443fcbdc4f85df307322a09
Jae Lee <=>
2023-03-05 01:20:39 +0900

    Authentication, Authorization 필터 추가로 인한 변경, HttpSecurity 옵션 변경

--- orderneat/src/main/java/com/orderneat/orderneat/config/SecurityConfig.java
+++ orderneat/src/main/java/com/orderneat/orderneat/config/SecurityConfig.java
@@ -1,58 +1,55 @@
 package com.orderneat.orderneat.config;
 
-import com.orderneat.orderneat.service.MemberService;
+import com.orderneat.orderneat.infrastructure.auth.JwtUtils;
+import com.orderneat.orderneat.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Override
-//    protected void configure(HttpSecurity httpSecurity) throws Exception {
-//        httpSecurity.formLogin()
-//                .loginPage("/member/login")
-//                .defaultSuccessUrl("/")
-//                .usernameParameter("email")
-//                .failureUrl("/member/login/error")
-//                .and()
-//                .logout()
-//                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
-//                .logoutSuccessUrl("/");
-//
-//        httpSecurity.authorizeRequests()
-//                .mvcMatchers("/", "/api/**", "/member/**", "/fonts/**", "/css/**", "/js/**",
-//                        "/fonts/**", "/images/**", "/icons/**").permitAll()
-//                .anyRequest().authenticated();
-//        httpSecurity.csrf().disable();
-//    }
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
+    private final JwtUtils jwtUtils;
+
     @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf()
-                .disable();
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable();
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers("/login", "/api/member/v1.0/join").permitAll();
+        http.authorizeRequests().antMatchers("/api/store/v1.0/register").hasAnyRole("USER", "STORE");
+        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean(), jwtUtils));
+        http.addFilterBefore(new CustomAuthorizationFilter(jwtUtils), UsernamePasswordAuthenticationFilter.class);
     }
 
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(memberService)
-//                .passwordEncoder(passwordEncoder());
-//    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsServiceImpl)
+                .passwordEncoder(passwordEncoder());
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
