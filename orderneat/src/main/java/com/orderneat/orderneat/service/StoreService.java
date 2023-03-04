e105e5ae8e6c9334ece2381ac42c2522e3dba93e
Jae Lee <=>
2023-03-05 01:23:26 +0900

    서비스에서 dto를 반환하지 않도록 변경

--- orderneat/src/main/java/com/orderneat/orderneat/service/StoreService.java
+++ orderneat/src/main/java/com/orderneat/orderneat/service/StoreService.java
@@ -1,34 +1,42 @@
 package com.orderneat.orderneat.service;
 
 
+import com.orderneat.orderneat.domain.Member;
 import com.orderneat.orderneat.domain.Store;
 import com.orderneat.orderneat.dto.store.StoreRegisterRequest;
 import com.orderneat.orderneat.dto.store.StoreRegisterResponse;
+import com.orderneat.orderneat.exception.OeException;
 import com.orderneat.orderneat.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class StoreService {
 
     private final StoreRepository storeRepository;
 
     @Transactional
-    public StoreRegisterResponse save(final StoreRegisterRequest request) {
+    public Long save(Member member, StoreRegisterRequest request) {
+        log.info("======== STORE SAVE REQUEST ========= {}", member.toString());
+        if (member == null) {
+            throw new OeException();
+        }
         Store store = Store.builder()
-                .name(request.getStoreName())
-                .category(request.getStoreCategory())
-                .address(request.getStoreAddress())
-                .address(request.getStoreContact())
+                .name(request.getName())
+                .member(member)
+                .category(request.getCategory())
+                .address(request.getAddress())
+                .contact(request.getContact())
                 .build();
         storeRepository.save(store);
-        StoreRegisterResponse response = new StoreRegisterResponse();
-        response.setId(store.getId());
-        return response;
+        return store.getId();
     }
-
 
 }
