667b2622edf06b5cc93c3c233a2df5e74a10eb7c
Jae Lee <=>
2023-03-05 01:22:41 +0900

    Spring Data Jpa 전환

--- orderneat/src/main/java/com/orderneat/orderneat/repository/StoreRepository.java
+++ orderneat/src/main/java/com/orderneat/orderneat/repository/StoreRepository.java
@@ -1,28 +1,12 @@
 package com.orderneat.orderneat.repository;
 
 import com.orderneat.orderneat.domain.Store;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import java.util.List;
+import java.util.Optional;
 
-@Repository
-@RequiredArgsConstructor
-public class StoreRepository {
 
-    private final EntityManager em;
+public interface StoreRepository extends JpaRepository<Store, Long> {
 
-    public void save(Store store){
-        em.persist(store);
-    }
-
-    public Store findOne(Long id){
-        return em.find(Store.class, id);
-    }
-
-    public List<Store> findAll(){
-        return em.createQuery("select s from Store s")
-                .getResultList();
-    }
+    Optional<Store> findStoreByMemberId(Long id);
 }
