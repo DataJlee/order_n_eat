39ad7d60a2cb9ef85208e705f80f411bdd628b17
Jae Lee <=>
2023-03-05 01:15:54 +0900

    테스트 코드 리팩토링

--- orderneat/src/test/java/com/orderneat/orderneat/service/MemberServiceTest.java
+++ orderneat/src/test/java/com/orderneat/orderneat/service/MemberServiceTest.java
@@ -1,68 +1,72 @@
 package com.orderneat.orderneat.service;
 
+
 import com.orderneat.orderneat.domain.Member;
-import com.orderneat.orderneat.domain.Role;
 import com.orderneat.orderneat.dto.member.MemberJoinRequest;
 import com.orderneat.orderneat.exception.member.DuplicateMemberException;
-import com.orderneat.orderneat.repository.MemberRepository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.orderneat.orderneat.exception.member.MemberNotFoundException;
+import com.orderneat.orderneat.repository.MemberRepositoryLegacy;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 
-import static org.junit.Assert.*;
+import java.util.Optional;
 
-@RunWith(SpringRunner.class)
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 @Transactional
 public class MemberServiceTest {
 
     @Autowired
-    MemberRepository memberRepository;
+    MemberRepositoryLegacy memberRepository;
     @Autowired
     MemberService memberService;
-    @Autowired
-    PasswordEncoder passwordEncoder;
 
     @Test
-    public void join() {
+    public void join() throws DuplicateMemberException {
         //given
-        MemberJoinRequest req = new MemberJoinRequest();
-        req.setEmail("new@gmail.com");
-        req.setPassword("pass1234");
+        MemberJoinRequest request = createMemberJoinRequest("test@email.com", "1234");
         //when
-        Long savedId = memberService.join(req);
+        Long savedId = memberService.join(request);
         //then
-        assertEquals(savedId, memberRepository.findOne(savedId).getId());
+        assertMemberIsJoined(savedId);
     }
 
-    @Test(expected = DuplicateMemberException.class)
-    public void validateDuplicateMember() throws DuplicateMemberException {
+    @Test
+    public void validateDuplicateMember() {
         //given
-        MemberJoinRequest req = new MemberJoinRequest();
-        req.setEmail("jaek1997@gmail.com");
-        req.setPassword("1234");
-
-        memberService.join(req);
+        MemberJoinRequest request = createMemberJoinRequest("test@email.com", "1234");
+        memberService.join(request);
         //when
-        MemberJoinRequest req2 = new MemberJoinRequest();
-        req2.setEmail("jaek1997@gmail.com");
-        req2.setPassword("1234");
+        MemberJoinRequest sameRequest = createMemberJoinRequest("test@email.com", "1234");
 
-        Member member2 = Member.builder()
-                .email(req2.getEmail())
-                .password(passwordEncoder.encode(req2.getPassword()))
-                .role(Role.USER)
-                .build();
-
-        memberService.join(req2);
-
-        memberService.validateDuplicateMember(member2);
         //then
-        fail("예외 발생 x, 테스트 실패");
+        assertThrows(DuplicateMemberException.class, ()-> memberService.join(sameRequest));
     }
+
+    @Test
+    void findMemberByEmail() {
+        //given
+        //when
+        Long joinedId = memberService.join(createMemberJoinRequest("test@email.com", "1234"));
+        Member memberFound = memberService.findMemberByEmail("test@email.com");
+        //then
+        assertEquals(joinedId, memberFound.getId());
+        assertThrows(MemberNotFoundException.class, ()-> memberService.findMemberByEmail("no@naver.com"));
+    }
+
+    //    사용자메서드 START
+    MemberJoinRequest createMemberJoinRequest(String email, String password){
+        MemberJoinRequest req = new MemberJoinRequest(email, password);
+        return req;
+    }
+
+    void assertMemberIsJoined(Long id) {
+        assertEquals(id, memberRepository.findOne(id).getId());
+    }
+    //    사용자메서드 END
 }
