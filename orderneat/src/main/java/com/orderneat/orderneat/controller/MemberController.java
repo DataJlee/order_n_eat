73f100a749b99c7e54f1424c7287a4f4699df881
Jae Lee <=>
2023-03-05 01:12:53 +0900

    thymeleaf controller 주석처리(사용안함)

--- orderneat/src/main/java/com/orderneat/orderneat/controller/MemberController.java
+++ orderneat/src/main/java/com/orderneat/orderneat/controller/MemberController.java
@@ -1,8 +1,6 @@
 package com.orderneat.orderneat.controller;
 
-import com.orderneat.orderneat.domain.Member;
-import com.orderneat.orderneat.dto.MemberJoinFormDTO;
-import com.orderneat.orderneat.dto.StoreRegistFormDTO;
+import com.orderneat.orderneat.dto.member.MemberJoinRequest;
 import com.orderneat.orderneat.service.MemberService;
 import com.orderneat.orderneat.service.StoreService;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Controller
@@ -24,41 +21,40 @@ import javax.validation.Valid;
 
 public class MemberController {
 
-//    public final MemberService memberService;
-//    public final StoreService storeService;
-//    public final PasswordEncoder passwordEncoder;
-//
-//    @GetMapping("/member/login")
-//    public String login(){
-//        return "store_member/member-login-pg";
-//    }
-//
-//    @GetMapping("/member/login/error")
-//    public String loginError(Model model){
-//        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
-//        return "store_member/member-login-pg";
-//    }
-//
-//    @GetMapping("/member/join")
-//    public String joinForm(Model model){
-//        model.addAttribute("memberJoinFormDto", new MemberJoinFormDTO());
-//        return "store_member/member-join-pg";
-//    }
-//
-//    @PostMapping("/member/join")
-//    public String memberJoin(@ModelAttribute("memberJoinFormDto") @Valid MemberJoinFormDTO memberJoinFormDto, BindingResult bindingResult, Model model){
-//        log.info("회원가입 페이지 가입하기 SUBMIT");
-//        if (bindingResult.hasErrors()){
-//            log.info(bindingResult.toString());
-//            return "store_member/member-join-pg";
-//        }
-//        try{
-//            Member member = Member.createMember(memberJoinFormDto, passwordEncoder);
-//            memberService.join(member);
-//        }catch (IllegalStateException e){
-//            model.addAttribute("errorMessage", e.getMessage());
-//            return "store_member/member-join-pg";
-//        }
-//        return "redirect:/";
-//    }
+    public final MemberService memberService;
+    public final StoreService storeService;
+    public final PasswordEncoder passwordEncoder;
+
+    @GetMapping("/member/login")
+    public String login(){
+        return "store_member/member-login-pg";
+    }
+
+    @GetMapping("/member/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
+        return "store_member/member-login-pg";
+    }
+
+    @GetMapping("/member/join")
+    public String joinForm(Model model){
+        model.addAttribute("MemberJoinRequest", new MemberJoinRequest());
+        return "store_member/member-join-pg";
+    }
+
+    @PostMapping("/member/join")
+    public String memberJoin(@ModelAttribute("MemberJoinRequest") @Valid MemberJoinRequest request, BindingResult bindingResult, Model model){
+        log.info("회원가입 페이지 가입하기 SUBMIT");
+        if (bindingResult.hasErrors()){
+            log.info(bindingResult.toString());
+            return "store_member/member-join-pg";
+        }
+        try{
+            memberService.join(request);
+        }catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "store_member/member-join-pg";
+        }
+        return "redirect:/";
+    }
 }
