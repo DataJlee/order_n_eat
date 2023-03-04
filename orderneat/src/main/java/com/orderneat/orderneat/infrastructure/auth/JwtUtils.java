64b8eb7e61a36343f4cae8b4e1f585dba03499a9
Jae Lee <=>
2023-03-05 01:11:47 +0900

    jwt 토큰 방식 인증 유틸 클래스 리팩토링

--- orderneat/src/main/java/com/orderneat/orderneat/infrastructure/auth/JwtUtils.java
+++ orderneat/src/main/java/com/orderneat/orderneat/infrastructure/auth/JwtUtils.java
@@ -1,75 +1,113 @@
 package com.orderneat.orderneat.infrastructure.auth;
 
-import com.orderneat.orderneat.exception.auth.ExpiredTokenException;
-import com.orderneat.orderneat.exception.auth.InvalidTokenException;
-import io.jsonwebtoken.*;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
-import java.util.Date;
-import java.util.Map;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtUtils {
 
-    private final String secretKey = "dafeafjaieaghi213113ihhdfiaodhfaievbaevuahheiahrueha4ueaeahuilh2314iu15r";
-    private final long expireLength = 30;
-    private final JwtParser jwtParser;
+    @Value("${jwt.secret}")
+    private String secretKey;
+    @Value("${jwt.accessTokenExpireLengthInMinutes}")
+    private Integer accessTokenExpireLengthInMinutes;
+    @Value("${jwt.refreshTokenExpireLengthInMinutes}")
+    private Integer refreshTokenExpireLengthInMinutes;
 
 
-    public JwtUtils(){
-        this.jwtParser = Jwts.parser().setSigningKey(secretKey);
+    public Map<String, String> createTokens(HttpServletRequest request, Authentication authentication) throws IOException {
+        User user = (User) authentication.getPrincipal();
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey.getBytes());
+
+        String access_token = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + accessTokenExpireLengthInMinutes*60*1000))
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim("roles", user.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .sign(algorithm);
+
+        String refresh_token = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + refreshTokenExpireLengthInMinutes*60*1000))
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim("roles", user.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .sign(algorithm);
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", access_token);
+        tokens.put("refresh_token", refresh_token);
+
+        return tokens;
     }
 
-    public String createToken(Map<String, Object> payload){
-        Claims claims = Jwts.claims(payload);
-        Date currentTime = new Date();
-        Date validThrough = new Date(currentTime.getTime() + expireLength);
+    public void validateToken(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        if (request.getServletPath().equals("/login")) {
+            log.info("========= login request {} =======", request.getServletPath());
+            filterChain.doFilter(request, response);
+        }else {
+            log.info(request.getServletPath());
+            String authorizationHeader = request.getHeader(AUTHORIZATION);
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                log.info("Token 유입 {}", authorizationHeader);
+                try {
+                    String tokenToVerify = authorizationHeader.substring("Bearer ".length());
+                    log.info("=== tokenToVerify {}", tokenToVerify);
+                    Algorithm algorithm = Algorithm.HMAC256(secretKey.getBytes());
+                    JWTVerifier jwtVerifier = JWT.require(algorithm).build();
+                    DecodedJWT decodedJWT = jwtVerifier.verify(tokenToVerify);
+                    log.info("=== decodedJWT {}", decodedJWT);
+                    String username = decodedJWT.getSubject();
+                    String[] roles = decodedJWT.getClaim("roles").asArray(String.class);
+                    Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+                    stream(roles).forEach(role -> authorities.add(new SimpleGrantedAuthority(role)));
+                    UsernamePasswordAuthenticationToken authenticationToken =
+                            new UsernamePasswordAuthenticationToken(username, null, authorities);
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                    filterChain.doFilter(request, response);
+                }catch (Exception e) {
+                    response.setHeader("token error", e.getMessage());
+                    response.setStatus(FORBIDDEN.value());
+                    Map<String, String> tokenErrors = new HashMap<>();
+                    tokenErrors.put("error_message", e.getMessage());
+                    response.setContentType(APPLICATION_JSON_VALUE);
+                    new ObjectMapper().writeValue(response.getOutputStream(), tokenErrors);
+                }
 
-        return Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(currentTime)
-                .setExpiration(validThrough)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
-                .compact();
-    }
-
-    public void validateToken(String token){
-        try{
-            jwtParser.parseClaimsJwt(token);
-        }catch (ExpiredJwtException e){
-            throw new ExpiredTokenException();
-        }catch (JwtException e){
-            throw new InvalidTokenException();
+            }else {
+                log.info("Authorization token does not exist");
+                filterChain.doFilter(request, response);
+            }
         }
     }
-
-    public String getPayload(String token){
-        return jwtParser.parseClaimsJwt(token).getBody().getSubject();
-    }
-
-    public static PayloadBuilder payloadBuilder(){
-        return new PayloadBuilder();
-    }
-
-    public static class PayloadBuilder{
-
-        private final Claims claims;
-
-        private PayloadBuilder(){
-            this.claims = Jwts.claims();
-        }
-
-        public PayloadBuilder setSubject(String subject){
-            claims.setSubject(subject);
-            return this;
-        }
-
-        public Map<String, Object> build(){
-            return claims;
-        }
-    }
-
-
 }
