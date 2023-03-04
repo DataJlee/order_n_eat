eb04993063a1838e53b76b653a23655034fbee65
Jae Lee <=>
2023-03-05 01:16:58 +0900

    Spring Data Jpa Repository 최초 커밋

--- orderneat/src/main/java/com/orderneat/orderneat/repository/MenuRepository.java
+++ orderneat/src/main/java/com/orderneat/orderneat/repository/MenuRepository.java
@@ -1,32 +1,8 @@
 package com.orderneat.orderneat.repository;
 
 import com.orderneat.orderneat.domain.Menu;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import java.util.List;
 
-@Repository
-@RequiredArgsConstructor
-public class MenuRepository {
-
-    private final EntityManager em;
-
-    public void save(Menu menu){
-        if (menu.getId() == null){
-            em.persist(menu);
-        }else {
-            em.merge(menu);
-        }
-    }
-
-    public Menu findOne(Long id){
-        return em.find(Menu.class, id);
-    }
-
-    public List<Menu> findAll(){
-        return em.createQuery("select m from Menu m", Menu.class)
-                .getResultList();
-    }
+public interface MenuRepository extends JpaRepository<Menu, Long> {
 }
