c788a7711d01390a7ae9ae265b4afd80672a0cc1
Jae Lee <=>
2023-03-05 01:15:30 +0900

    이메일로 회원찾기 기능 추가

--- orderneat/src/main/java/com/orderneat/orderneat/service/MemberService.java
+++ orderneat/src/main/java/com/orderneat/orderneat/service/MemberService.java
@@ -4,30 +4,30 @@ import com.orderneat.orderneat.domain.Member;
 import com.orderneat.orderneat.domain.Role;
 import com.orderneat.orderneat.dto.member.MemberJoinRequest;
 import com.orderneat.orderneat.exception.member.DuplicateMemberException;
+import com.orderneat.orderneat.exception.member.MemberNotFoundException;
 import com.orderneat.orderneat.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class MemberService{
+public class MemberService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public Long join(final MemberJoinRequest memberJoinRequest) {
+    public Long join(MemberJoinRequest memberJoinRequest) {
 
         Member member = Member.builder()
                     .email(memberJoinRequest.getEmail())
                     .password(passwordEncoder.encode(memberJoinRequest.getPassword()))
-                    .role(Role.USER)
+                    .role(Role.ROLE_USER)
                     .build();
         validateDuplicateMember(member);
         memberRepository.save(member);
@@ -37,13 +37,17 @@ public class MemberService{
 
     public void validateDuplicateMember(Member member) {
 
-        List<Member> memberByEmail = memberRepository.findByEmail(member.getEmail());
+        Optional<Member> memberByEmail = memberRepository.findMemberByEmail(member.getEmail());
         if (!memberByEmail.isEmpty()){
             throw new DuplicateMemberException();
         }
     }
 
-
-
-
+    public Member findMemberByEmail(String email) {
+        Optional<Member> member = memberRepository.findMemberByEmail(email);
+        if (member.isEmpty()){
+            throw new MemberNotFoundException();
+        }
+        return member.get();
+    }
 }
