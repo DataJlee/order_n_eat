ce8483a1c27429acffa038af8d03cc1f71a8a266
Jae Lee <=>
2023-03-05 01:08:27 +0900

    주석제거

--- orderneat/src/main/java/com/orderneat/orderneat/controller/HomeController.java
+++ orderneat/src/main/java/com/orderneat/orderneat/controller/HomeController.java
@@ -1,18 +1,13 @@
 package com.orderneat.orderneat.controller;
 
-import com.orderneat.orderneat.domain.Member;
-import com.orderneat.orderneat.repository.MemberRepository;
 import com.orderneat.orderneat.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.security.Principal;
 
 @Controller
 @Slf4j
@@ -21,8 +16,8 @@ public class HomeController {
 
     private final MemberService memberService;
 
-    @RequestMapping("/")
-    public String home(Model model, @AuthenticationPrincipal User user){
-        return "index";
-    }
+//    @RequestMapping("/")
+//    public String home(Model model, @AuthenticationPrincipal User user){
+//        return "index";
+//    }
 }
